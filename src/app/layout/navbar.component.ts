e4eed053a6dfc51598dda8b0231e38309a35bebd
Tyler Briskie <tylerbriskie@gmail.com>
2019-05-11 16:34:33 -0600

    finishing feature work

--- src/app/layout/navbar.component.ts
+++ src/app/layout/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _authService: AuthService) { }
 
   ngOnInit() {
   }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,8 @@ export class AuthService {
   private _registerUrl = "http://localhost:1337/clients";
   private _loginUrl = "http://localhost:1337/login";
   private _getClientInfo = "http://localhost:1337/clients/getLoggedIn";
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private _router: Router) { }
 
   registerClient(client){
     return this.http.post<any>(this._registerUrl, client)
@@ -24,6 +26,11 @@ export class AuthService {
 
   loggedIn() {
     return !!localStorage.getItem('token');
+  }
+
+  logoutUser(){
+    localStorage.removeItem('token');
+    this._router.navigate(['/home']);
   }
 
   getToken() {
