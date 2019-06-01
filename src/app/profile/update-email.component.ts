f6beb15c4a28b327497815c63a6c9230d8ff3e97
Tyler Briskie <tylerbriskie@gmail.com>
2019-06-01 17:45:00 -0600

    updates to profile styling and email validation

--- src/app/profile/update-email.component.ts
+++ src/app/profile/update-email.component.ts
@@ -9,14 +9,10 @@ import {ErrorStateMatcher} from "@angular/material";
 export class UpdateEmailComponent {
   @Input() currentEmail: string;
   @Output() cancel = new EventEmitter();
-
-  newEmail = '';
-
-  constructor(){}
-
-  updateEmailForm = new FormControl('', [
-    Validators.required,
+  emailRegEx = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  newEmail = new FormControl('', [
     Validators.email,
+    Validators.pattern(this.emailRegEx),
   ]);
   emailMatcher = new EmailErrorStateMatcher();
 
@@ -26,9 +22,10 @@ export class UpdateEmailComponent {
 
   submitUpdateEmail(){
     event.preventDefault();
-    alert('the email you chose was ' + this.newEmail);
+    alert('the email you chose was ' + this.newEmail.value);
   }
 }
+
 export class EmailErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     const isSubmitted = form && form.submitted;
--- src/app/profile/update-password.component.ts
+++ src/app/profile/update-password.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from "@angular/forms";
+import {ErrorStateMatcher, MatSnackBar} from "@angular/material";
 
 @Component({
   selector: 'update-password-form',
@@ -6,21 +8,36 @@ import {Component} from '@angular/core';
 })
 
 export class UpdatePasswordComponent {
+
+  @Output() cancel = new EventEmitter();
   currentPassword = '';
   newPassword = '';
   newPasswordConfirm = '';
 
-  constructor(){
+  constructor(private _snackBar: MatSnackBar){
 
+  }
+
+  cancelUpdate(){
+    this.cancel.emit();
   }
 
   submitUpdatePassword(){
     event.preventDefault();
     if (this.newPassword.length < 8){
       this._snackBar.open('Password Must Be 8 Characters', "OK", {duration: 2500});
+      return;
     }
-    if (this.newPassword === this.newPasswordConfirm){
-      this._snackBar.open('Passwords Match', "OK", {duration: 2500});
+    if (this.newPassword !== this.newPasswordConfirm){
+      this._snackBar.open('Passwords Must Match', "OK", {duration: 2500});
+      return;
     }
+  }
+}
+
+export class PasswordErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
   }
 }
